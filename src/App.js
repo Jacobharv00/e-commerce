502aee1ba8d085c605fc2f9027e09913dc53c6e5
jacob <jacobharv00@gmail.com>
2021-11-05 17:00:15 -0600

    setup, display, style navbar and ecommerce items with images

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react'
+import { Products, Navbar } from './components'
+import { commerce } from './lib/commerce'
 
-function App() {
+const App = () => {
+  const [ products, setProducts ] = useState( [] )
+
+  const fetchProducts = async () => {
+    const { data } = await commerce.products.list()
+    setProducts( data )
+  }
+
+  useEffect( () => {
+    fetchProducts()
+  }, [] )
+
+  console.log( products )
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Navbar />
+      <Products products={ products } />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
