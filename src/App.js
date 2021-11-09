8617e1cedefb2232a110794f3da5281002b0669d
jacob <jacobharv00@gmail.com>
2021-11-09 12:43:07 -0700

    added increment and decrement and completly empty cart of items

--- src/App.js
+++ src/App.js
@@ -18,8 +18,23 @@ const App = () => {
   }
 
   const handleAddToCart = async ( productId, quantity ) => {
-    const item = await commerce.cart.add( productId, quantity )
-    setCart( item.cart )
+    const { cart } = await commerce.cart.add( productId, quantity )
+    setCart( cart )
+  }
+
+  const handleUpdateCartQty = async ( productId, quantity ) => {
+    const { cart } = await commerce.cart.update( productId, { quantity } )
+    setCart( cart )
+  }
+
+  const handleRemoveFromCart = async ( productId ) => {
+    const { cart } = await commerce.cart.remove( productId )
+    setCart( cart )
+  }
+
+  const handleEmptyCart = async () => {
+    const { cart } = await commerce.cart.empty()
+    setCart( cart )
   }
 
   useEffect( () => {
@@ -38,7 +53,15 @@ const App = () => {
         <Routes>
           <Route exact path='/'
             element={ <Products products={ products } onAddToCart={ handleAddToCart } /> } />
-          <Route exact path="/cart" element={ <Cart cart={ cart } /> } />
+          <Route exact path="/cart"
+            element={
+              <Cart
+                cart={ cart }
+                handleUpdateCartQty={ handleUpdateCartQty }
+                handleRemoveFromCart={ handleRemoveFromCart }
+                handleEmptyCart={ handleEmptyCart }
+              />
+            } />
         </Routes>
       </div>
     </Router>
--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -4,13 +4,21 @@ import useStyles from './styles'
 import CartItem from './CartItem/CartItem'
 import { Link } from 'react-router-dom'
 
-const Cart = ( { cart } ) => {
+const Cart = ( { cart, handleUpdateCartQty, handleRemoveFromCart, handleEmptyCart } ) => {
   const classes = useStyles()
 
   const EmptyCart = () => (
-    <Typography variant='subtitle1'>
+    <Typography variant='h2'>
       You have no items in your shopping cart.
-      <Link to='/' className={ classes.link }>Start Adding Some!</Link>
+      <Link to='/' className={ classes.link }>
+        <Button
+          size='medium'
+          type='button'
+          variant='contained'
+          color='secondary'>
+          Start Adding Some!
+        </Button>
+      </Link>
     </Typography>
   )
 
@@ -19,7 +27,11 @@ const Cart = ( { cart } ) => {
       <Grid container spacing={ 3 }>
         { cart.line_items.map( item => (
           <Grid item xs={ 12 } sm={ 4 } key={ item.id }>
-            <CartItem item={ item } />
+            <CartItem
+              item={ item }
+              onUpdateCartQty={ handleUpdateCartQty }
+              onRemoveFromCart={ handleRemoveFromCart }
+            />
           </Grid>
         ) ) }
       </Grid>
@@ -28,7 +40,7 @@ const Cart = ( { cart } ) => {
           Subtotal: { cart.subtotal.formatted_with_symbol }
         </Typography>
         <div>
-          <Button className={ classes.emptyButton } size='large' type='button' variant='contained' color='secondary'>
+          <Button className={ classes.emptyButton } size='large' type='button' variant='contained' color='secondary' onClick={ handleEmptyCart }>
             Empty Cart
           </Button>
           <Button className={ classes.checkoutButton } size='large' type='button' variant='contained' color='primary'>
@@ -44,7 +56,7 @@ const Cart = ( { cart } ) => {
   return (
     <Container>
       <div className={ classes.toolbar } />
-      <Typography className={ classes.title } variant='h3' gutterBottom>
+      <Typography className={ classes.title } variant='h1' gutterBottom>
         Shopping Cart
       </Typography>
       { !cart.line_items.length ? <EmptyCart /> : <FilledCart /> }
