22637ca1b82a5b0dbda4d16f485b1643785e7ec2
jacob <jacobharv00@gmail.com>
2021-12-01 15:40:08 -0700

    playing with react lazyload for images

--- src/components/Products/Product/Product.js
+++ src/components/Products/Product/Product.js
@@ -2,17 +2,20 @@ import React from 'react'
 import { Card, CardMedia, CardContent, CardActions, Typography, IconButton } from '@material-ui/core'
 import { AddShoppingCart } from '@material-ui/icons'
 import useStyles from './styles'
+import LazyLoad from 'react-lazyload'
 
 const Product = ( { product, onAddToCart } ) => {
   const classes = useStyles()
 
   return (
     <Card className={ classes.root }>
-      <CardMedia
-        className={ classes.media }
-        image={ product.image.url }
-        title={ product.name }
-      />
+      <LazyLoad height={ 400 }>
+        <CardMedia
+          className={ classes.media }
+          image={ product.image.url }
+          title={ product.name }
+        />
+      </LazyLoad>
       <CardContent>
         <div className={ classes.cardContent }>
           <Typography variant='h4' gutterBottom style={ { fontFamily: 'Merienda, cursive', } }>
@@ -34,6 +37,7 @@ const Product = ( { product, onAddToCart } ) => {
         </IconButton>
       </CardActions>
     </Card>
+
   )
 }
 
